7d1a587551094ce19afde3a443c092811a32e265
Brent Chuang <brent@sct.com.au>
2012-09-27 14:35:53 +1000

    Add apipie-rails initializer

---
+++ config/initializers/apipie.rb
@@ -0,0 +1,13 @@
+# Apipie.configure do |config|
+#   config.app_name = 'API Documentation Test app'
+#   config.doc_base_url = '/apidoc'
+#   config.api_base_url = '/api/v1'
+#   config.validate = true
+#   # config.markup = Apipie::Markup::Markdown.new
+#   config.reload_controllers = true
+#   config.api_controllers_matcher = File.join(Rails.root, 'app', 'controllers', '**','*.rb')
+#   config.app_info = <<-DOC
+#     This is where you can inform user about your application and API
+#     in general.
+#   DOC
+# end
