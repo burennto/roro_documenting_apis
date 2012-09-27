85dfb50ffed4256f751b6614a4cb5522d05cc93e
Brent Chuang <brent@sct.com.au>
2012-09-27 14:47:55 +1000

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+twain     = Person.create!(:name => 'Mark Twain',        :occupation => 'Author',     :fictional => false)
+churchill = Person.create!(:name => 'Winston Churchill', :occupation => 'Politician', :fictional => false)
+wayne     = Person.create!(:name => 'Bruce Wayne',       :occupation => 'Batman',     :fictional => true)
+
+twain.quotations.create!(:quote => 'The reports of my death have been greatly exaggerated.')
+twain.quotations.create!(:quote => 'Go to Heaven for the climate, Hell for the company.')
+twain.quotations.create!(:quote => 'Be careful about reading health books. You may die of a misprint.')
+twain.quotations.create!(:quote => "Don't let schooling interfere with your education.")
+
+churchill.quotations.create!(:quote => 'We make a living by what we get, but we make a life by what we give.')
+churchill.quotations.create!(:quote => "You have enemies? Good. That means you've stood up for something, sometime in your life.")
+churchill.quotations.create!(:quote => 'History will be kind to me for I intend to write it.')
